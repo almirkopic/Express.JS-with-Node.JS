a0693a9fffbeb5806489573730f2ddb4e751880a
Almir Kopic <133537874+almirkopic@users.noreply.github.com>
2023-07-06 02:57:02 +0200

    Add files via upload
    localhost:3000, running simple calculator app, nodemon calculator.js, setup by terminal VS

---
+++ project-calculator/calculator.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+ 
+
+app.get("/", function (request, response) {
+    response.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+
+    var num1= Number(req.body.n1);
+    var num2 = Number(req.body.n2);
+
+    var result = num1 + num2;
+
+res.send("The result of the calculation is " + result);
+});
+
+// Start the server 3000 port
+app.listen(3000, function (){
+    console.log("Server started on 3000")
+});
